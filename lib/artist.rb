e9040a740a5b8c82873469091ea100291a3b1e83
Conor Hamilton <conhamil19@gmail.com>
2018-05-29 03:51:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist 
-  attr_accessor :name, :genre, :artist
+  attr_accessor :name
   @@all = []
   
   def self.all 
@@ -12,8 +12,7 @@ class Artist
   end 
   
   def new_song(song_name, genre)
-    song = Song.new(song_name, self, genre)
-    song.artist = self 
+    Song.new(song_name, self, genre)
   end 
   
   def songs 
