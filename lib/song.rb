5208c7424795ddbcf34f396909e6d5c0fb11b2cd
Conor Hamilton <conhamil19@gmail.com>
2018-05-29 03:32:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,16 @@
 class Song 
+  attr_accessor :name, :genre. :artist 
+  @@all = [] 
+  
+  def intialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+
 end 
