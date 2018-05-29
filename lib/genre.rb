6c0d7a1fcb1fe009766dc48cf290a90811141820
Conor Hamilton <conhamil19@gmail.com>
2018-05-29 03:33:04 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre 
+  attr_accessor :genre, :artist 
+  attr_reader :name 
+  @@all = []
+  
+  def self.all 
+    @all 
+  end 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.artist = self 
+  end 
+  
+  def songs 
+    Songs.all.select do |song|
+      song.artist == self 
+    end 
+  
+  def genres 
+    Songs.all.collect do |song|
+      song.genre
+    end 
+end 
