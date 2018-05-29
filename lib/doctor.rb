f75b5558c2e34bd219568d6c41ac776620faba70
Conor Hamilton <conhamil19@gmail.com>
2018-05-29 04:12:37 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,17 @@
+class Doctor 
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+end 
